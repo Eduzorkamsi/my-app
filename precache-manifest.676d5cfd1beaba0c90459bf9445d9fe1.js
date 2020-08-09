db9d04c01b9a283f6eea6cfeb67b3f02796273a2
Eduzorkamsi <eduzorkamsi@gmail.com>
2020-08-09 22:57:16 +0100

    Updates

---
+++ precache-manifest.676d5cfd1beaba0c90459bf9445d9fe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77af498fe84d479f41f92b2b521693d2",
+    "url": "/Eduzorkamsi/my-app.git/index.html"
+  },
+  {
+    "revision": "be77c1a5fa61097cd17e",
+    "url": "/Eduzorkamsi/my-app.git/static/css/main.d827332f.chunk.css"
+  },
+  {
+    "revision": "8ec41427f7ba06323399",
+    "url": "/Eduzorkamsi/my-app.git/static/js/2.1b0b6b68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Eduzorkamsi/my-app.git/static/js/2.1b0b6b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be77c1a5fa61097cd17e",
+    "url": "/Eduzorkamsi/my-app.git/static/js/main.7f927a81.chunk.js"
+  },
+  {
+    "revision": "0e797d84e0bfded456c4",
+    "url": "/Eduzorkamsi/my-app.git/static/js/runtime-main.73ebd1d4.js"
+  }
+]);
